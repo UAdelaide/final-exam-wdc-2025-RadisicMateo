4904e6114b0ed231a107b128e88d8e0cb4218d4c
Mateo <mateoradisic2005@gmail.com>
2025-06-20 07:16:01 +0000

    [automatic commit] 2025-06-20 07:16:01 UTC 1.147.118.220

--- part1/routes/walkRequests.js
+++ part1/routes/walkRequests.js
@@ -27,7 +27,7 @@ router.get('/open', async function(req,rs) {
         `);
         res.json(rows);
     } catch (err) {
-        res.status(500)
+        res.status(500).json({ error: 'fail'})
     }
 
 
