d064ea18d1fb8899251cd5bf52019d0dd682e3d1
Mateo <mateoradisic2005@gmail.com>
2025-06-20 07:16:43 +0000

    [automatic commit] 2025-06-20 07:16:43 UTC 1.147.118.220

--- part1/app.js
+++ part1/app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('./routes/dogs');
+var walkrequestsRouter = require()
 var walkersRouter = require('./routes/walkers');
+
 
 var app = express();
 
