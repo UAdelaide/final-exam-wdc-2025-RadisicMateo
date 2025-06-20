92ef86326dcfdc53aced106f1071ef98e085dceb
Mateo <mateoradisic2005@gmail.com>
2025-06-20 07:15:18 +0000

    [automatic commit] 2025-06-20 07:15:18 UTC 1.147.118.220

--- part1/routes/walkRequests.js
+++ part1/routes/walkRequests.js
@@ -12,19 +12,20 @@ const pool = mysql.createPool({
 
 router.get('/open', async function(req,rs) {
     try {
-        const [rows] = await pool.query('
-        SELECT
-            wr.request_id,
-            d.name AS dog_name,
-            wr.requested_time,
-            wr.duration_minutes,
-            wr.location,
-            u.username AS owner_username
-        FROM WalkRequests wr
-        JOIN Dogs d ON wr.dog_id = d.dog_id
-        JOIN Users u ON d.owner_id = u.user_id
-        WHERE wr.status='open'
-        ')
+        const [rows] = await pool.query(`
+            SELECT
+              wr.request_id,
+              d.name AS dog_name,
+              wr.requested_time,
+              wr.duration_minutes,
+              wr.location,
+              u.username AS owner_username
+            FROM WalkRequests wr
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.owner_id = u.user_id
+            WHERE wr.status = 'open'
+        `);
+        res.json
     }
 
 
