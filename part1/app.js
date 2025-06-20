97957f178cebffc631fd04d0c4915c077cd9c809
Mateo <mateoradisic2005@gmail.com>
2025-06-20 07:17:48 +0000

    [automatic commit] 2025-06-20 07:17:48 UTC 1.147.118.220

--- part1/app.js
+++ part1/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/dogs', dogsRouter);
-app.use('/api/walk')
+app.use('/api/walkrequests', walkrequestsRouter);
 app.use('/api/walkers', walkersRouter);
 
 module.exports = app;
