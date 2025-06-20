4f979948b78a1d1dbd5f294312ca566a1628db42
Mateo <mateoradisic2005@gmail.com>
2025-06-20 14:46:38 +0000

    [automatic commit] 2025-06-20 14:46:38 UTC 58.174.20.94

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,8 +60,10 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req,res) => {
   req.session.destroy(err => {
     if (err) {
-      return res.status(500).json({ error: 'Logout failed'})
+      return res.status(500).json({ error: 'Logout failed' });
     }
+    res.clearCookie('connect.sid');
+    res.json
   })
 })
 
